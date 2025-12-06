db0f59e46dd1c143267b38af89c0b7fd30fba488
sanket batwal <batwalsanket@gmail.com>
2025-12-06 22:17:14 +0530

    chnage the image name

--- src/Componants/Projects/Projects.jsx
+++ src/Componants/Projects/Projects.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 // Replace these with your real project images
-import Project1 from "../../assets/project1.jpg";
-import Project2 from "../../assets/project2.jpg";
-import Project3 from "../../assets/project3.jpg";
+import Project1 from "../../assets/Project1.jpg";
+import Project2 from "../../assets/Project2.jpg";
+import Project3 from "../../assets/Project3.jpg";
 
 const Projects = () => {
   const projects = [
