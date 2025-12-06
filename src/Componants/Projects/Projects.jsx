1087836d6b8e6133f7fdba271f536c44a17b08be
sanket batwal <batwalsanket@gmail.com>
2025-12-06 22:25:07 +0530

    chnage the image name

--- src/Componants/Projects/Projects.jsx
+++ src/Componants/Projects/Projects.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 // Replace these with your real project images
-import Project1 from "../../assets/Project1.jpg";
-import Project2 from "../../assets/Project2.jpg";
-import Project3 from "../../assets/Project3.jpg";
+ 
 
 const Projects = () => {
   const projects = [
@@ -14,7 +12,7 @@ const Projects = () => {
       description:
         "A full-stack blog app with authentication, CRUD features, REST APIs, and MongoDB.",
       tech: ["Tailwindcss","JavaScript","React","Stripe","Node", "Express", "MongoDB"],
-      image: Project1,
+      image: "/Project1.jpg",
       github: "https://github.com/batwalsanket04/Handloom-Saree-E-commerce-WebApp",
       live: "https://handloom-saree-e-commerce-webapp-frontend-113c.onrender.com",
     },
@@ -24,7 +22,7 @@ const Projects = () => {
       description:
         "Expense Tracker Web-App built with MERN Stack Tools.authentication,CRUD operstions ,REST APIs and  All data handled in the mongoDB",
       tech: ["React", "Tailwind", "JavaScript","Node.Js","Express.Js","MongoDB"],
-      image: Project2,
+      image: "/Project2",
       github: "https://github.com/",
       live: "https://yourportfolio.vercel.app",
     },
@@ -34,7 +32,7 @@ const Projects = () => {
       description:
         "The project showcases a fully functional frontend interface that allows users to explore property listings, view details, and navigate through a visually appealing layout designed for real-world real estate platforms.",
       tech: ["TailwindCss","React.js","Web3Forms"],
-      image: Project3,
+      image: "/Project3",
       github: "https://github.com/batwalsanket04/RealEstate-WebApp",
       live: "https://real-estate-web-app-beta.vercel.app/",
     },
